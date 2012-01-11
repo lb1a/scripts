378503cea8c2412a69f4c581369a60092db24946
lb1a <luke.b3103@gmail.com>
2012-01-11 16:36:03 +0100

    importing change encoding script on bash

---
+++ changeCharset.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+MOMENTARY_DIR=$(pwd)
+if [ "$1" != "--debug" ]; then
+CHOSEN_DIR=$1
+cd $CHOSEN_DIR
+for i in $(find . -name "*html"); do
+	iconv -t utf8//IGNORE $i | sed -e 's/CHARSET=iso8859-1/CHARSET=UTF-8/g' > $i.1
+	mv $i.1 $i
+	echo $i" converted"
+done
+cd $MOMENTARY_DIR
+else
+cd $CHOSEN_DIR = $2
+for i in $(find . -name "*html"); do
+        TEST=$(iconv -t utf8//IGNORE $i | grep "iconv") 2>&1 > /dev/null
+	if [ "$TEST" != "" ]; then
+        echo $i
+	fi
+done
+
+fi
