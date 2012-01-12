a60d8dadb21240147ad1341b352268c37886f397
lb1a <luke.b3103@gmail.com>
2012-01-12 09:28:58 +0100

    using .html and not ignoring error silently

--- changeCharset.sh
+++ changeCharset.sh
@@ -3,8 +3,8 @@ MOMENTARY_DIR=$(pwd)
 if [ "$1" != "--debug" ]; then
 CHOSEN_DIR=$1
 cd $CHOSEN_DIR
-for i in $(find . -name "*html"); do
-	iconv -t utf8//IGNORE $i | sed -e 's/CHARSET=iso8859-1/CHARSET=UTF-8/g' > $i.1
+for i in $(find . -name "*.html"); do
+	iconv -t utf8 $i | sed -e 's/CHARSET=iso8859-1/CHARSET=UTF-8/g' > $i.1
 	mv $i.1 $i
 	echo $i" converted"
 done
